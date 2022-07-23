32532ee028958f0a1daed6edc6c818df0d377737
Justin Reisz <justin@wesleyreisz.com>
2022-07-23 18:15:37 -0400

    inital project structure

---
+++ Project1CS480/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ToyTetragraphHash.Tests")]
+namespace ToyTetragraphHash
+{
+    class ToyTetragraphHash
+    {
+        static void Main(string[] args)
+        {
+            ToyTetragraphHash hash = new ToyTetragraphHash();
+            System.Console.WriteLine(hash.outTitle());
+        }
+        internal String outTitle()
+        {
+            return "ToyTetragraphHash";
+        }
+    }
+}
---
+++ ToyTetragraphHash.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace ToyTetragraphHash.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestTitle()
+    {
+        ToyTetragraphHash hash = new ToyTetragraphHash();
+        Assert.AreEqual(hash.outTitle(), "ToyTetragraphHash");
+        Assert.Pass();
+    }
+}
