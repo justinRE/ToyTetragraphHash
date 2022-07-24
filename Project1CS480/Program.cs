b3117a08eab90f35e699d8f7fa3a9a53b4cec98f
JustinRE <justin@wesleyreisz.com>
2022-07-23 21:19:40 -0400

    iterating on the two-dimensional array creation

--- Project1CS480/Program.cs
+++ Project1CS480/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.CompilerServices;
+using System;
 
 [assembly: InternalsVisibleTo("ToyTetragraphHash.Tests")]
 namespace ToyTetragraphHash
@@ -14,12 +15,49 @@ namespace ToyTetragraphHash
         {
             return "ToyTetragraphHash";
         }
-        internal List<string[,]> stringToBlocks(String input) {
+        internal List<string[,]> stringToBlocks(String input)
+        {
             List<String[,]> blocks = new List<String[,]>();
-            String[,] block = new String[3,3];
+            String[,] block = new String[3, 3];
             blocks.Add(block);
 
             return blocks;
+        }
+
+        internal string cleanString(string input)
+        {
+
+            string result = new string(input.Where(c => !char.IsPunctuation(c)).ToArray());
+            return result.Replace(" ", "").ToLower();
+        }
+
+        internal string[,] createTwoDimensionArrayFromString(string input)
+        {
+            if (input.Length != 16){
+                throw new Exception("Invalid length"); //expection
+            }
+
+            int counter = 0;
+            int row = 0;
+            int col = 0;
+
+            foreach (char item in input)
+            {
+                counter++;
+                if (counter == 1) col = 0;
+
+                Console.Out.Write(item);
+                Console.Out.Write("[" + row+ "," + col + "]");
+
+                col++;
+                if ((counter % 4) == 0 && counter !=0)
+                {
+                    row++;
+                    col = 0;
+                }
+               
+            }
+            return new string[0, 0];
         }
     }
 }
--- ToyTetragraphHash.Tests/UnitTest1.cs
+++ ToyTetragraphHash.Tests/UnitTest1.cs
@@ -27,5 +27,38 @@ public class Tests
 
     }
 
+    [Test]
+    public void TestCleanString()
+    {
+        ToyTetragraphHash hash = new ToyTetragraphHash();
+        String result = hash.cleanString("My name is justin.!@#");
+        Assert.AreEqual("mynameisjustin", result);
+
+    }
+    [Test]
+    public void TestCreateTwoDimensionArrayFromString()
+    {
+        ToyTetragraphHash hash = new ToyTetragraphHash();
+        String[,] result = hash.createTwoDimensionArrayFromString("ABCDEFGHIJKLMNOP");
+        Assert.AreEqual(4,result.GetLength(0));
+        Assert.AreEqual(4, result.GetLength(1));
+
+    }
+
+    [Test]
+    public void TestInputSizeforTwoDimensionArrayFromStringTooLong()
+    {
+        ToyTetragraphHash hash = new ToyTetragraphHash();
+        Assert.Throws<Exception>(
+             delegate { hash.createTwoDimensionArrayFromString("ABCDEFGHIJKLMNOPQ"); });
+    }
+
+    [Test]
+    public void TestInputSizeforTwoDimensionArrayFromStringTooShort()
+    {
+        ToyTetragraphHash hash = new ToyTetragraphHash();
+        Assert.Throws<Exception>(
+             delegate { hash.createTwoDimensionArrayFromString("ABCDEF"); });
+    }
 
 }
